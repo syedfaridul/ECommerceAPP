b84e3686636d72e546ba699ab73eaa0a9321249c
ubunturifa <jstrfaheem065@gmail.com>
2019-09-19 01:29:16 +0530

    SignUp activity,

--- app/src/main/java/com/flover/rifaecom/MainActivity.java
+++ app/src/main/java/com/flover/rifaecom/MainActivity.java
@@ -2,13 +2,28 @@ package com.flover.rifaecom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button test = findViewById(R.id.signUpButton);
+        test.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.signUpButton:
+                Intent registration = new Intent(getBaseContext(), SignUp.class);
+                startActivity(registration);
+        }
     }
 }
